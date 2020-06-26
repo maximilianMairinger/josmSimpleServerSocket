492b4f98bf9f5247d2774201403a9ec0bd3506ea
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-06-26 15:33:05 +0200

    ws working

--- app/src/josmSimpleServerSocket.ts
+++ app/src/josmSimpleServerSocket.ts
@@ -1,5 +1,18 @@
-dependencyImports
+import Socket from "ws"
+ 
 
-export default function() {
+
+
+export default function(port: number = 8080) {
+  const wss = new Socket.Server({ port });
+
+  wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
+      console.log('received: %s', message);
+    });
   
+    ws.send('something');
+  });
+
+  console.log("WebSocket running on port: ", port)
 }
